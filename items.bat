9bcc83c03d35ab4f67ecf017f1fed7c972414e4b
Leo Neves <enfimleo@gmail.com>
2025-03-06 20:17:04 -0300

    jsons

--- items.bat
+++ items.bat
@@ -1,5 +1,7 @@
 @echo off
 type nul > items.txt
-for /r %%i in (*) do (
-    if not "%%~nxi"=="items.bat" if not "%%~nxi"=="items.txt" echo %%~pi%%~nxi >> items.txt
+for /r %%i in (*.txt,*.json,*.pdf) do (
+    if not "%%~nxi"=="items.bat" if not "%%~nxi"=="items.txt" (
+        echo %%~pi%%~nxi | findstr /i /v "\\\.git\\" >> items.txt
+    )
 )
